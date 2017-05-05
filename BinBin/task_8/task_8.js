5ef04baeb60305f993c905c70ad6b799afbfcfe5
YLiJingan <954047253@qq.com>
2017-05-05 16:27:19 +0800

    revision

--- BinBin/task_8/task_8.js
+++ BinBin/task_8/task_8.js
@@ -1,38 +1,35 @@
 window.onload =(function(){
 	var arr =[];
-	var last;
-	var flag = false;
-	var timer = null;
 	var root = document.getElementsByClassName("root")[0];
 	var btn = document.getElementsByClassName("btn_group")[0];
 	var prebtn = document.getElementById("preorder");
-	var search_val = document.getElementById("search").value;
+	var postbtn = document.getElementById("postorder");
+	var search = document.getElementById("search");
 	var search_btn = document.getElementById("search_btn");
 	var divs = document.getElementsByTagName("div");
 
 	/*遍历过程的显示*/
 	function show(){
-		var i = 0;
-		timer = setInterval(function(){
-			if(i == arr.length-1){
-				flag = false;
-				clearInterval(timer);
-			}
-			if(last){
-				last.style.background = "#fff";
-			}
-			arr[i].style.background = "blue";
-			last = arr[i];i++;
-		},500);
+        var i=0;
+        arr[i].style.backgroun="#0000ff";
+        var timer=setInterval(function(){
+            i++;
+            if(i>=arr.length){
+                clearInterval(timer);
+                arr[arr.length-1].style.background="#fff";
+            }else{
+                arr[i-1].style.background="#fff";
+                arr[i].style.background="#0000ff";
+            }
+        },500);
 	}
+	/*重置颜色*/
 	function reset(){
-		arr=[];
-		clearInterval(timer);
-		if(last){
-			last.style.background = "#fff";
-		}
+		for(var i=0;i<arr.length;i++){
+            arr[i].style.backgroundColor="#fff";
+        }
 	}
-
+	/*先序遍历*/
 	function preorder(node){
 		if(node !== null){
 			arr.push(node);
@@ -41,12 +38,54 @@ window.onload =(function(){
 			}
 		}
 	}
+	/*后序遍历*/
+	function postorder(node){
+		if(node !== null){
+			for(var i=0;i<node.children.length;i++){
+				postorder(node.children[i]);
+			}
+			arr.push(node);
+		}
+	}
+	/*查找过程显示*/
+    function searchFun(word){
+        var i=0;
+        var timer=setInterval(function(){
+            i++;
+            if(search.value==""){
+                clearInterval(timer);
+                alert("请输入内容")
+                return;
+            }
+            if(i>=arr.length){
+                clearInterval(timer);
+                arr[arr.length-1].style.background="#fff";
+                alert("未查找到内容");
+                return;
+            }else{
+                arr[i-1].style.background="#fff";
+                arr[i].style.background="#ff0000";
+            }
+            if(arr[i].childNodes[0].nodeValue.search(word)!=-1){
+            	alert(arr[i].childNodes[0]);
+                alert("查找到内容");
+                clearInterval(timer);
+            }
+        },500)
+    }
 	prebtn.onclick = function(){
-		if(!flag){
-			flag = true;
 			reset();
 			preorder(root);
 			show();
-		}
+	}
+	postbtn.onclick = function(){
+			postorder(root);
+			reset();
+			show();
+	}
+	search_btn.onclick = function(){
+		reset();
+		preorder(root);
+		searchFun(search.value);
 	}
 })()
